150f5f9cb6d4e24b8e94cfafb4daca90ec062237
ashish739293 <108934924+ashish739293@users.noreply.github.com>
2022-08-18 15:13:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var a=prompt("Enter Your Name");
  var b=prompt("Enter Your DOB.");
-  	if(a== "khushi" || b=="19/08/2003")
+  	if(a== "khushi" || b=="19/08/2002" ||a=="khushi sharma")
   	{
   	    window.location.href="start.html";
   	}
