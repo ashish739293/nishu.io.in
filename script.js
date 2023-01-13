347d20cc85ec2d2385c4ec0822197c981a83a79a
ashish739293 <108934924+ashish739293@users.noreply.github.com>
2023-01-13 23:33:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
-var a=prompt("Enter Your Name");
- var b=prompt("Enter Your DOB.");
-  	if(a== "khushi" || b=="19/08/2002" ||a=="khushi sharma")
-  	{
+       alert("Welcome ! Please  Press OK ");
   	    window.location.href="start.html";
-  	}
-  	else
-  	{
-  		alert("invalid details");
-  		window.location.href="index.html";
-  	}
+  	
   	
